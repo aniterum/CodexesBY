1171b7799bea811c0b4555f8763e24844b3fa76b
aniterum <aniterum@gmail.com>
2016-09-07 15:07:32 +0300

    Первая запись скриптов и иконок

---
+++ dump_laws.sh
@@ -0,0 +1,66 @@
+#!/bin/bash
+
+IFS=$(echo -en "\n\b")
+
+DATA="bank|HK0000441|Банковский кодекс
+budget|Hk0800412|Бюджетный кодекс
+water|HK9800191|Водный кодекс
+air|Hk0600117|Воздушный кодекс
+civil|HK9800218|Гражданский кодекс
+civil_proc|HK9900238|Гражданский процессуальный кодекс
+housing|Hk1200428|Жилищный кодекс
+voting|HK0000370|Избирательный кодекс
+vater_transport|Hk0200118|Кодекс внутреннего водного транспорта
+marriage_family|HK9900278|Кодекс о браке и семье
+ground|Hk0800425|Кодекс о земле
+underground|Hk0800406|Кодекс о недрах
+court_and_judge|Hk0600139|Кодекс о судоустройстве и статусе судей
+admin|Hk0300194|КоАП
+educational|Hk1100243|Кодекс об образовании
+seafaring_trade|HK9900321|Кодекс торгового мореплавания
+wood|HK0000420|Лесной кодекс
+taxing_common|Hk0200166|Налоговый кодекс (Общая часть)
+taxing_special|Hk0900071|Налоговый кодекс (Особенная часть)
+admin_proc|Hk0600194|ПИКоАП
+job|HK9900296|Трудовой кодекс
+criminal|HK9900275|Уголовный кодекс
+criminal_exec|HK0000365|Уголовно-исполнительный кодекс
+criminal_proc|HK9900295|Уголовно-процессуальный кодекс
+property_proc|HK9800219|Хозяйственный процессуальный кодекс"
+
+minimumsize=90000
+mkdir -p laws_ashx
+mkdir -p laws_xml
+
+for i in $DATA; do 
+  actualsize=80000
+  NAME=`cut -d"|" -f1 <<< $i`
+  ID=`cut -d"|" -f2 <<< $i`
+  COMMENT=`cut -d"|" -f3 <<< $i`
+  echo $NAME $COMMENT
+  
+
+  #Пробуем несколько раз, пока не получим файл, т.к. их API глючит
+  while [ $actualsize -le $minimumsize ]; do
+    sleep 5
+    wget "http://etalonline.by/api/Document.ashx?type=Text&regnum="$ID"&date=none&history=0&queryid=&print=false&RNList=&checked=false&RNPA=&typeload=text" -O /tmp/law_tmp
+    #curl 'http://etalonline.by/api/Document.ashx' --data 'type=Text&regnum='$ID'&date=none&history=0&queryid=&print=false&RNList=&checked=false&RNPA=&typeload=text' -o /tmp/law_tmp
+    actualsize=$(du -b /tmp/law_tmp | cut  -f1)
+    echo $actualsize
+  done
+
+  xmllint --xpath "string(//Doc/Text)" <<< cat /tmp/law_tmp > /tmp/law_tmp1
+  xmllint -format -recover -encode utf-8 /tmp/law_tmp1 > laws_ashx/${NAME}.xml
+  #Чистим от <br/> <sup> </sup> будут мешать XML парсингу
+  sed -i -e 's/<br\/>/|/g;s/<sup>/^/g;s/<\/sup>//g; s/<b>//g; s/<\/b>//g
+            /.*<span class\=\"onesymbol\"/s/<\/span>//g; s/<span class\=\"onesymbol\">//g
+            /.*<span class\=\"articlec\"/s/<\/span>//g;  s/<span class\=\"articlec\" style\=\"\">//g
+            /.*<span class\=\"rednoun\"/s/<\/span>//g;   s/<span class\=\"rednoun\" style\=\"\">//g
+            /.*<span class\=\"fontstyle/s/<\/span>//g;   s/<span class\=\"fontstyle1[45]\" style\=\"\">//g' laws_ashx/${NAME}.xml
+
+  ./parse.py "laws_ashx/${NAME}.xml" "laws_xml/${NAME}.xml"
+
+done
+
+./parse.py --make-links
+
---
+++ parse.py
@@ -0,0 +1,223 @@
+#!/usr/bin/env python3
+
+from xml.etree import ElementTree as ET
+
+def ParseAshx(ifile, ofile):
+    
+    x = ET.parse(ifile).getroot()
+    sec = x[1][0]
+
+    if sec.get('class') != 'Section1':
+        exit
+
+    def addChild(parent, tag, attrs = None):
+        elem = ET.Element(tag)
+        if attrs != None:
+            for key in attrs:
+                if attrs[key] != None:
+                    elem.set(key, attrs[key])
+        parent.append(elem)
+        return elem
+
+
+    doc = ET.Element("document")
+    info = ET.Element("docinfo")
+    changes = ET.Element("changes")
+    text = ET.Element("text")
+
+    doc.append(info)
+    doc.append(text)
+    info.append(changes)
+
+    curr_razdel = None
+    curr_article = None
+    curr_chapter = None
+
+    ch_id = 0
+
+    for i in sec.iter():
+        tag = i.get('class')
+        if tag == "titlek": #Название
+            if i.text.strip() == "": #Особый случай для банковского кодекса
+                _title = ""
+                for _i in i.iter():
+                    if _i.tag == "span":
+                        if _i.get('class') in ["name", "promulgator"]:
+                            _title += _i.text
+                            
+                addChild(info, 'info', {'class':'title', 'text':__title})
+            else:
+                addChild(info, 'info', {'class':'title', 'text':i.text})
+        elif tag == "datepr": #Дата принятия
+            addChild(info, 'info', {'class':'date', 'text':i.text})
+        elif tag == "number": #Номер документа
+            addChild(info, 'info', {'class':'number', 'text':i.text})
+        elif tag == "prinodobren": #Принят и одобрен
+            addChild(info, 'info', {'class':'getpower', 'text':i.text})
+        elif tag == "changeutrs": #Утратил силу
+            addChild(info, 'info', {'class':'lostpower', 'text':i.text})
+        elif tag == "changeadd": #Добавлено изменение или дополнение
+            ch_id += 1
+            addChild(changes, 'info', {'class':'changes', 'text':i.text, 'id':str(ch_id)})
+        elif tag == "zagrazdel": #Раздел
+            curr_chapter = None
+            if i.get('id') != None:
+                curr_razdel = addChild(text, 'razdel', {'id':i.get('id'), 'text':i.text})
+        elif tag == "chapter": #Глава
+            if curr_razdel == None:
+                curr_razdel = text
+            curr_chapter = addChild(curr_razdel, 'chapter', {'id':i.get('id'), 'text':i.text})
+        elif tag in ["article", "articleintext"]: #Статья
+            if curr_chapter != None:
+                curr_article = addChild(curr_chapter, 'article', {'id':i.get('id'), 'text':i.text})
+            elif curr_razdel != None:
+                curr_article = addChild(curr_razdel, 'article', {'id':i.get('id'), 'text':i.text})
+           
+        elif tag in ["newncpi", "contenttext"]: 
+            if curr_article != None:
+                if i.text != None:
+                    if i.text.strip() != "":
+                        addChild(curr_article, 'p', {'text':i.text})
+                    
+        elif tag == "point":
+            if curr_article != None:
+                if i.text != None:
+                    if i.text.strip() != "":
+                        addChild(curr_article, 'p', {'id':i.get('id'), 'text':i.text, 'class':'point'})
+
+        elif tag == "underpoint":
+            if curr_article != None:
+                if i.text != None:
+                    if i.text.strip() != "":
+                        addChild(curr_article, 'p', {'id':i.get('id'), 'text':i.text, 'class':'underpoint'})
+   
+        elif tag == "comment":
+             if curr_article != None:
+                 if i.text != None:
+                     if i.text.strip() != "":
+                         addChild(curr_article, 'p', {'id':i.get('id'), 'text':i.text, 'class':'comment'})
+
+        elif tag == "snoski":
+             if curr_article != None:
+                 if i.text != None:
+                     if i.text.strip() != "":
+                         addChild(curr_article, 'p', {'id':i.get('id'), 'text':i.text, 'class':'snoski'})
+
+        elif tag == "rekviziti":
+             if i.text != None:
+                 if curr_article != None:
+                     if i.text.startswith("_") and i.text.endswith("_"):
+                         addChild(curr_article, 'p', {'id':i.get('id'), 'text':i.text, 'class':'rekvizit'})
+
+        elif tag == "titlep":
+            #TODO: Приложение к кодексу
+            pass
+
+        
+        else:
+            if not tag in ["snoskiv", None, "snoskiline",
+                           "contentword", "part", "articlec", "newncpi2",
+                           "Section1", "changei", "post", "pers", "preamble",
+                           "promulgator", "newncpi0", "name", "append1",
+                           "paragraph", "podrazdel", "numheader", "nonumheader", #образуют слишком большую вложенность
+                           "table10", #Таблицы в особой части налогового кодекса
+                           ]:
+                print(tag)
+
+
+    tree = ET.ElementTree(doc)
+    tmpFile = '/tmp/tmp_xml.xml'
+    try:
+        tree.write(tmpFile, encoding="utf-8", xml_declaration=True)
+
+        command = 'xmllint -format -recover "%s" > "%s"'
+        os.system(command % (tmpFile, ofile))
+    except:
+        ET.dump(doc)
+        
+
+
+import sys
+import os
+import hashlib
+
+
+action = None
+infile = ""
+ofile = ""
+source_dir = 'laws_ashx'
+parsed_dir = 'laws_xml'
+
+if len(sys.argv) > 1:
+    if sys.argv[1] == "--all":
+        action = "all"
+    elif sys.argv[1] == "--make-list":
+        action = "make_list"
+    else:
+        infile = sys.argv[1]
+        outfile = sys.argv[2]
+
+
+from os.path import isfile, exists, realpath
+
+if action == "all":
+    for root, dirs, files in os.walk(source_dir):
+        for file in files:
+            print(file)
+            ParseAshx(source_dir + "/" + file, parsed_dir + "/" + file)
+            
+elif action == "make_list":
+    import zlib
+    files_attrib = []
+    for root, dirs, files in os.walk(parsed_dir):
+        for file in files:
+            _tmp = {}
+            fname = parsed_dir+"/"+file
+            file_stat = os.stat(fname) #File size
+            data = open(fname, 'rb').read()
+            zipped = zlib.compress(data)
+            open('laws_zlib/' + file + ".zlib", 'wb').write(zipped)
+            
+            file_hash = hashlib.sha1(data).hexdigest() #SHA1-sum
+
+            _root = ET.parse(fname).getroot()
+            for i in _root.iter():
+                if i.tag == "info":
+                    if i.get('class') == 'title':
+                        file_title = i.get('text').strip()
+
+            
+            _tmp["size"]  = str(file_stat.st_size)
+            _tmp["packed"] = str(len(zipped))
+            _tmp["name"]  = file + ".zlib"
+            _tmp["hash"]  = file_hash
+            _tmp["title"] = file_title.title()
+           
+            files_attrib.append(_tmp)
+
+            
+    links = ET.Element('links')
+    links.set("URL", "https://github.com/aniterum/CodexesBY/raw/master/laws_zlib/")
+
+    for i in files_attrib:
+        codex_info = ET.Element('codex')
+        links.append(codex_info)
+        codex_info.attrib = i
+            
+    tree = ET.ElementTree(links)
+    tmpFile = '/tmp/tmp_xml.xml'
+    try:
+        tree.write(tmpFile, encoding="utf-8", xml_declaration=True)
+
+        command = 'xmllint -format  "%s" > "%s"'
+        os.system(command % (tmpFile, 'links.xml'))
+    except:
+        ET.dump(links)
+
+else:
+    if exists(infile):
+        print(infile)
+        ParseAshx(infile, outfile)
+
+
+
